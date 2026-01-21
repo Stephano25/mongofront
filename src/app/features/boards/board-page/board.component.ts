4b571ea399e39cd22976e080e1a02ed4f9d00b7d
Stephano2 <dazzremie@gmail.com>
2026-01-21 22:46:34 +0300

    corréction des module

--- src/app/features/boards/board-page/board.component.ts
+++ src/app/features/boards/board-page/board.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardViewModel } from './board.viewmodel';
 import { BoardService } from './board.service';
+import { ListComponent } from '../../lists/list.component';
 
 @Component({
-  selector: 'app-board',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './board.component.html',
-  providers: [BoardViewModel, BoardService],
+ selector: 'app-board',
+ standalone: true,
+ imports: [CommonModule, ListComponent],
+ templateUrl: './board.component.html',
+ providers: [BoardViewModel, BoardService],
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
+  board: any;
+ 
   constructor(public vm: BoardViewModel) {}
+
+  ngOnInit() {
+    this.vm.boards$.subscribe((boards) => {
+      this.board = boards[0];
+    });
+ }
 }
